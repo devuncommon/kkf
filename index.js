c302beb30f97307f04d942404a2a580c5459523d
devuncommon <dev@uncommon.nl>
2025-03-06 10:23:06 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+<!-- Vimeo SRC -->
+<script>
+document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll(".video-cover").forEach((e=>{const t=e.getAttribute("data-video");if(t){const o=e.querySelector("video");e.querySelector("source").setAttribute("src",t),o.load()}}))}));
+</script>
+
+<!-- Image Mask -->
+<script>
+document.addEventListener("DOMContentLoaded",(function(){gsap.registerPlugin(ScrollTrigger),gsap.utils.toArray(".image-mask").forEach((function(r){gsap.fromTo(r,{y:"0%"},{y:"-100%",ease:"power3.out",duration:1.5,scrollTrigger:{trigger:r,start:"top 100%",end:"top 0%",scrub:!1}})}))}));
+</script>
+
+<!-- Button Stagger -->
+<script>
+function initButtonCharacterStagger(){document.querySelectorAll("[data-button-animate-chars]").forEach((t=>{const e=t.textContent;t.innerHTML="",[...e].forEach(((e,n)=>{const a=document.createElement("span");a.textContent=e,a.style.transitionDelay=.003*n+"s"," "===e&&(a.style.whiteSpace="pre"),t.appendChild(a)}))}))}document.addEventListener("DOMContentLoaded",(()=>{initButtonCharacterStagger()}));
+</script>
+
+<!-- Accordion -->
+<script>
+document.addEventListener("DOMContentLoaded",(()=>{document.querySelectorAll("[data-accordion-css-init]").forEach((t=>{const a="true"===t.getAttribute("data-accordion-close-siblings");t.addEventListener("click",(c=>{const e=c.target.closest("[data-accordion-toggle]");if(!e)return;const o=e.closest("[data-accordion-status]");if(!o)return;const s="active"===o.getAttribute("data-accordion-status");o.setAttribute("data-accordion-status",s?"not-active":"active"),a&&!s&&t.querySelectorAll('[data-accordion-status="active"]').forEach((t=>{t!==o&&t.setAttribute("data-accordion-status","not-active")}))}))}))}));
+</script>
+
+<!-- Scroll Top -->
+<script>
+$(".scroll-top").on("click touchstart",(function(o){o.preventDefault(),lenis.scrollTo(0,{duration:1.5,easing:o=>o<.5?8*o*o*o*o:1-Math.pow(-2*o+2,4)/2})}));
+});
+</script>
